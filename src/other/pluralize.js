97a2c4b63ee40f2a16a95471c0f9506c318fb362
freearhey <free.arhey@gmail.com>
2019-03-29 21:19:27 +0300

    Added support for word arrays and the option to include a number in the output

--- src/other/pluralize.js
+++ src/other/pluralize.js
@@ -3,21 +3,24 @@ import util from '../util/index'
 /**
  * 'item' => 'items'
  *
- * @params
- *  an array of strings corresponding to
- *  the single, double, triple ... forms of the word to
- *  be pluralized. When the number to be pluralized
- *  exceeds the length of the args, it will use the last
- *  entry in the array.
+ * @param {String|Array} word
+ * @param {Object} options
  *
- *  e.g. ['single', 'double', 'triple', 'multiple']
  */
 
-function pluralize (value) {
-  var args = util.toArray(arguments, 1)
-  return args.length > 1
-    ? (args[value - 1] || args[args.length - 1])
-    : (args[0] + (value === 1 ? '' : 's'))
+function pluralize (value, word, options) {
+  options = options || {}
+  var output = ''
+  var includeNumber = options.includeNumber != null ? options.includeNumber : false
+  if(includeNumber === true) output += value + ' '
+  if(!value && value !== 0 || !word) return output
+  if(Array.isArray(word)) {
+    output += word[value - 1] || word[word.length - 1]
+  } else {
+    output += word + (value === 1 ? '' : 's')
+  }
+
+  return output
 }
 
 export default pluralize
