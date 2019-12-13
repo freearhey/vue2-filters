07b8ce90f1d491464df21ed0e53568f643210c24
freearhey <free.arhey@gmail.com>
2019-12-13 19:40:44 +0300

    Update build

--- dist/vue2-filters.js
+++ dist/vue2-filters.js
@@ -700,6 +700,16 @@ function number_number(value, format, options) {
   return config.sign + int + fraction;
 }
 
+Math.sign = function (x) {
+  x = +x;
+
+  if (x === 0 || isNaN(x)) {
+    return x;
+  }
+
+  return x > 0 ? 1 : -1;
+};
+
 function parseNumber(num) {
   return {
     float: Math.abs(parseFloat(num)),
