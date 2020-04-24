8e85b028d5561bbd3ebb94f1fbd500db82361816
freearhey <free.arhey@gmail.com>
2020-04-25 00:19:38 +0300

    Filter must return a value multiplied by 100 (#86)

--- src/other/percent.js
+++ src/other/percent.js
@@ -1,25 +1,22 @@
 import util from '../util/index'
 
 /**
- * 12               => '12%'
- * 100              => '100%'
- * 1000             => '1000%'
+ * 1.2              => '120%'
+ * -0.2             => '-20%'
+ * 100              => '10000%'
+ * 1                => '100%'
  * 0.97             => '97%'
  *
  * @param {Number} value
  * @param {Number} decimals Decimal places (default: 2)
  */
 function percent(value, decimals) {
-    const globalOptions = (this && this.percent) ? this.percent : {}
-    decimals = util.exist(decimals) ? decimals : globalOptions.decimalDigits
-    decimals = (typeof decimals !== 'undefined') ? decimals : 0
-    value = value === null || isNaN(value) ? 0 : value
-    
-    if(value <= 1) {
-        return `${(value * 100).toFixed(decimals)}%`
-    }
-    
-    return `${value.toFixed(decimals)}%`
+  const globalOptions = this && this.percent ? this.percent : {}
+  decimals = util.exist(decimals) ? decimals : globalOptions.decimalDigits
+  decimals = typeof decimals !== 'undefined' ? decimals : 0
+  value = value === null || isNaN(value) ? 0 : value
+
+  return `${(value * 100).toFixed(decimals)}%`
 }
 
 export default percent
