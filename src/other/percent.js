2b4fcdb13814a25813c39b18fc5aa507e3ad8721
freearhey <free.arhey@gmail.com>
2020-04-25 17:22:09 +0300

    Added multiplier attribute

--- src/other/percent.js
+++ src/other/percent.js
@@ -10,13 +10,15 @@ import util from '../util/index'
  * @param {Number} value
  * @param {Number} decimals Decimal places (default: 2)
  */
-function percent(value, decimals) {
+function percent(value, decimals, multiplier) {
   const globalOptions = this && this.percent ? this.percent : {}
+  multiplier = util.exist(multiplier) ? multiplier : globalOptions.multiplier
+  multiplier = typeof multiplier !== 'undefined' ? multiplier : 100
   decimals = util.exist(decimals) ? decimals : globalOptions.decimalDigits
   decimals = typeof decimals !== 'undefined' ? decimals : 0
   value = value === null || isNaN(value) ? 0 : value
 
-  return `${(value * 100).toFixed(decimals)}%`
+  return `${(value * multiplier).toFixed(decimals)}%`
 }
 
 export default percent
