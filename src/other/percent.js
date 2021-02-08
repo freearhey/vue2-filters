92da6ac7890372cc56db551dbd18385b23cbaf03
Erfan <erfan.rezaee71@gmail.com>
2021-02-08 05:15:55 +0330

    Add decimal separator option

--- src/other/percent.js
+++ src/other/percent.js
@@ -9,16 +9,22 @@ import util from '../util/index'
  *
  * @param {Number} value
  * @param {Number} decimals Decimal places (default: 2)
+ * @param {Number} multiplier (default: 100)
+ * @params {Object} options
  */
-function percent(value, decimals, multiplier) {
+function percent(value, decimals, multiplier, options) {
   const globalOptions = this && this.percent ? this.percent : {}
+  options = options || globalOptions
   multiplier = util.exist(multiplier) ? multiplier : globalOptions.multiplier
   multiplier = typeof multiplier !== 'undefined' ? multiplier : 100
   decimals = util.exist(decimals) ? decimals : globalOptions.decimalDigits
   decimals = typeof decimals !== 'undefined' ? decimals : 0
+
+  const decimalSeparator = options.decimalSeparator != null ? options.decimalSeparator : '.'
+
   value = value === null || isNaN(value) ? 0 : value
 
-  return `${(value * multiplier).toFixed(decimals)}%`
+  return `${(value * multiplier).toFixed(decimals).replace('.', decimalSeparator)}%`
 }
 
 export default percent
