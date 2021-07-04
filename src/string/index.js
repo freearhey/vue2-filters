d531644effd0e24aaf03eb5b37b54c7ad7d3ede8
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-07-04 15:46:57 +0200

    five new filters with tests

--- src/string/index.js
+++ src/string/index.js
@@ -1,13 +1,19 @@
-import capitalize from './capitalize'
-import uppercase from './uppercase'
-import lowercase from './lowercase'
-import placeholder from './placeholder'
-import truncate from './truncate'
+import capitalize from "./capitalize";
+import uppercase from "./uppercase";
+import lowercase from "./lowercase";
+import placeholder from "./placeholder";
+import truncate from "./truncate";
+import repeat from "./repeat";
+import wrap from "./wrap";
+import reverse from "./reverse";
 
 export {
-  capitalize,
-  uppercase,
-  lowercase,
-  placeholder,
-  truncate
-}
+    capitalize,
+    uppercase,
+    lowercase,
+    placeholder,
+    truncate,
+    repeat,
+    wrap,
+    reverse,
+};
