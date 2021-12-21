ad8598625950b7f6b52e4705fd0686c565090086
Aleksandr Statciuk <github@oiya.xyz>
2021-12-21 13:33:06 +0300

    Update index.js

--- src/array/index.js
+++ src/array/index.js
@@ -2,7 +2,5 @@ import limitBy from "./limitBy";
 import filterBy from "./filterBy";
 import orderBy from "./orderBy";
 import find from "./find";
-import some from "./some";
-import every from "./every";
 
-export { limitBy, filterBy, orderBy, find, some, every };
+export { limitBy, filterBy, orderBy, find };
